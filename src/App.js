5bb6ab203066121282e3ce2d993fb823d51b6e82
Carolina Mauro <carolina.mauro@brosoft.com.ar>
2022-06-07 13:45:41 -0300

    agregando estilo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import logoCarolina from './images/Carolina.png'
+import Task from './components/Tasks';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app-tasks">
+      <div className='logo'>
+        <img
+          src={logoCarolina}
+           />
+      </div>
+      <div className='principal-tasks-list'>
+        <h1> MIS TAREAS </h1>
+        <Task text='Aprender React'/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../stylesheet/Task.css'
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
+function Task( { text, completed } ){
+  return (
+    <div className={ completed ? "task-container completed" : "task-container"}>
+      <div className="task-text">
+        { text }
+      </div>
+      <div className="task-icon-container">
+        <AiOutlineCloseCircle className="task-icon" />
+      </div>
+    </div>
+  );
+        
+}
+
+export default Task;
